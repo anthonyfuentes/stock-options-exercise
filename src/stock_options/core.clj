66fb613abecc8194a17d334dd2989015ff22b5be
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2020-04-10 10:54:16 -0700

    Implemlent -main

--- src/stock_options/core.clj
+++ src/stock_options/core.clj
@@ -1,6 +1,16 @@
-(ns stock-options.core)
+(ns stock-options.core
+  (:require [stock-options.input.cli :refer [parse-shifts]])
+  (:require [stock-options.input.csv :refer [vectorize]])
+  (:require [stock-options.labeling.vectors :refer [label-options]])
+  (:require [stock-options.casting.maps :refer [cast-options]])
+  (:require [stock-options.casting.numbers :refer [->number]])
+  (:require [stock-options.projection.contracts :refer [project-returns]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  [& [filepath shifts]]
+  (-> filepath
+      vectorize
+      label-options
+      cast-options
+      (project-returns (parse-shifts shifts))
+      println))
