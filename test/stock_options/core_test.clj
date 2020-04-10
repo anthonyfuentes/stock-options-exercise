64c468e202cd3de2ae32a38c6671d9b66817e693
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2020-04-10 05:32:40 -0700

    Scaffold app

---
+++ test/stock_options/core_test.clj
@@ -0,0 +1,7 @@
+(ns stock-options.core-test
+  (:require [clojure.test :refer :all]
+            [stock-options.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
